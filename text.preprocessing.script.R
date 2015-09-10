4a4cf4fc97ddf4d6af96b0d9fd1cf1abf68d48eb
mohabdel2013 <mohabdel2013@gmail.com>
2015-09-09 20:15:57 -0700

    Add text preprocessing R script

---
+++ text.preprocessing.script.R
@@ -0,0 +1,40 @@
+# Map 1-based optional input ports to variables
+dataset <- maml.mapInputPort(1) # class: data.frame
+
+library("tm")
+
+# Separate the label and tweet text 
+sentiment_label <- dataset[[1]]
+tweet_text      <- dataset[[2]]
+
+print("Convert to lowercase ....")
+preprocessed_tweet_text <- sapply(tweet_text, tolower)
+
+print("Remove Emails ....")
+email_regexp <- "[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?"
+preprocessed_tweet_text <- gsub(email_regexp, "EMAIL", preprocessed_tweet_text)
+
+print("Remove URLs ....")    
+url_regexp <- "(f|ht)(tp)(s?)(://)(.*)[.|/][^ ]+"
+preprocessed_tweet_text <- gsub(url_regexp, "URL", preprocessed_tweet_text)
+
+print("Replace punctuation, special characters and digits with space ....")
+preprocessed_tweet_text <- gsub("[^a-z]+", " ", preprocessed_tweet_text)
+
+print("Remove duplicate characters  ....")
+preprocessed_tweet_text <- gsub('([[:alpha:]])\\1+', '\\1\\1', preprocessed_tweet_text)
+
+print("Create corpus ....")
+theCorpus <- Corpus(VectorSource(preprocessed_tweet_text))
+
+print("Remove stopword characters  ....")
+theCorpus <- tm_map(theCorpus, removeWords, stopwords("english"))
+
+print("Word stemming ....")
+theCorpus <- tm_map(theCorpus, stemDocument, language = "english")                                         
+
+data.set <- data.frame(preprocessed_tweet_text=unlist(sapply(theCorpus, "[[", "content")), stringsAsFactors=F)                                       
+data.set <- cbind(sentiment_label, tweet_text, data.set)                   
+
+# Select data.frame to be sent to the output Dataset port
+maml.mapOutputPort("data.set")
